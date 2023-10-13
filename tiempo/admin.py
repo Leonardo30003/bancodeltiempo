299603c58a878a79f0d6a0b0cbce5e11620d11f3
Leonardo <lecarrionca@uide.edu.ec>
2023-10-12 19:35:04 -0500

    models

--- tiempo/admin.py
+++ tiempo/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Categorias
+from .models import Rol,Usuario,Cuenta,Categoria,Interes
 
 # Register your models here.
-admin.site.register(Categorias)
+admin.site.register(Usuario)
+admin.site.register(Cuenta)
+admin.site.register(Interes)
+admin.site.register(Rol)
+admin.site.register(Categoria)
--- tiempo/models.py
+++ tiempo/models.py
@@ -1,19 +1,48 @@
 from django.db import models
 
 # Create your models here.
-class Categorias(models.Model):
-    nombreCategorias = models.CharField(max_length=100)
+from django.db import models
 
 class Persona(models.Model):
-    choices_genero = [('h','Hombre'),('M','Mujer')]
-    nombres = models.CharField(verbose_name = "nombres", max_length=50)
-    apellidos = models.CharField(verbose_name ="apellidos", max_length=50)
-    genero = models.CharField(verbose_name ="genero", max_length=50, choices = choices_genero)
-    documento_identificacion = models.CharField(verbose_name ="cedula", max_length=50)
-    email = models.CharField(verbose_name ="email", max_length=50)
-    telefono = models.CharField(verbose_name ="telefono", max_length=50)
+    choices_genero = [('h', 'Hombre'), ('m', 'Mujer')]
+    nombres = models.CharField(verbose_name="nombres", max_length=50)
+    apellidos = models.CharField(verbose_name="apellidos", max_length=50)
+    genero = models.CharField(verbose_name="genero", max_length=1, choices=choices_genero)
+    documento_identificacion = models.CharField(verbose_name="cedula", max_length=50)
+    email = models.CharField(verbose_name="email", max_length=50)
+    telefono = models.CharField(verbose_name="telefono", max_length=50)
     fechaNacimiento = models.DateField(verbose_name="fecha Nacimiento")
-    direccion = models.CharField(verbose_name="direccion",max_length=150)
-    
-    def _str_(self):
-        return self.nombres 
+    direccion = models.CharField(verbose_name="direccion", max_length=150)
+
+    def __str__(self):
+        return self.nombres
+
+class Rol(models.Model):
+    choices_rol = [('administrador', 'Administrador'), ('cliente', 'Cliente')]
+    nombre_rol = models.CharField(verbose_name="nombreRol", max_length=50, choices=choices_rol)
+
+    def __str__(self):
+        return self.nombre_rol
+
+class Usuario(Persona):
+    nombre_usuario = models.CharField(verbose_name="usuario", max_length=50)
+    password = models.CharField(verbose_name="password", max_length=50)
+    rol = models.ManyToManyField(Rol, related_name="usuarios")
+       
+
+class Cuenta(models.Model):
+    usuarioCuenta = models.ManyToManyField(Usuario, related_name="cuentas")
+    fechaCreacion = models.DateField(verbose_name="fecha Creacion")
+    fechaActualizacion = models.DateField(verbose_name="fecha Actualizacion")
+    imagen = models.ImageField(verbose_name="imagen")
+    numero_horas = models.IntegerField(verbose_name="numeroHoras")
+
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(verbose_name="nombre Categoria", max_length=100)
+    def __str__(self):
+        return self.nombre_categoria
+
+class Interes(models.Model):
+    usuarioIntereses = models.ManyToManyField(Usuario, related_name="intereses")
+    categoria = models.ManyToManyField(Categoria, related_name="categorias")
+
