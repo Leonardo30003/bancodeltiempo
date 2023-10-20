a2f61bb9cfadd5aed50e97e67062303937f9a592
Leonardo <lecarrionca@uide.edu.ec>
2023-10-20 17:24:09 -0500

    ultimo

--- tiempo/views.py
+++ tiempo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializer import ServicioSerializers,CuentaSerializers
+from .models import Servicio,Cuenta
+
+# Create your views here.
+class ServicioViewSet(viewsets.ModelViewSet):
+    queryset = Servicio.objects.all()
+    serializer_class = ServicioSerializers
+
+class CuentaViewSet(viewsets.ModelViewSet):
+    queryset = Cuenta.objects.all()
+    serializer_class = CuentaSerializers
+
+class PersonaViewSet(viewsets.ModelViewSet):
+    queryset = Cuenta.objects.all()
+    serializer_class = CuentaSerializers
